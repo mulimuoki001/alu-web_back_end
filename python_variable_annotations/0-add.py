566e691b5ef2d455de5dd39075127f09cce4bc50
mulimuoki001 <b.muoki@alustudent.com>
2024-09-03 10:07:40 +0300

    documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python3
 
 
-# variable a and b should contain floats and the return value should be a float
+"""
+add
+---
+
+This function takes two float numbers, `a` and `b`, and
+returns their sum as a float.
+
+Parameters:
+    a (float): The first float number.
+    b (float): The second float number.
+
+Returns:
+    float: The sum of `a` and `b`.
+
+Example:
+    >>> add(2.0, 3.0)
+    5.0
+"""
+
+
 def add(a: float, b: float) -> float:
-    return a + b  # return the sum of a and b
+    """
+    Add two float numbers and return their sum.
+
+    Args:
+        a (float): The first float number.
+        b (float): The second float number.
+
+    Returns:
+        float: The sum of `a` and `b`.
+    """
+    return a + b
