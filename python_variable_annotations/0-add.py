f3edb21fbeed6c066814a3b40a5b37513c3c2273
mulimuoki001 <b.muoki@alustudent.com>
2024-09-03 10:04:31 +0300

    documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-def add(
-    a: float, b: float
-) -> (
-    float
-):  # variable a and b should contain floats and the return value should be a float
+
+
+# variable a and b should contain floats and the return value should be a float
+def add(a: float, b: float) -> float:
     return a + b  # return the sum of a and b
