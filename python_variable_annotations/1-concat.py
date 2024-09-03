d423d07ea0303fb9edfd5b8befe9b8b1ea1e33ea
mulimuoki001 <b.muoki@alustudent.com>
2024-09-03 10:13:38 +0300

    concantenation of two strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+"""
+Concatenate two strings and return the result.
+
+Args:
+    str1 (str): The first string to concatenate.
+    str2 (str): The second string to concatenate.
+
+Returns:
+    str: The concatenated string.
+
+Example:
+    >>> concat("Hello ", "World!")
+    'Hello World!'
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    return str1 + str2
