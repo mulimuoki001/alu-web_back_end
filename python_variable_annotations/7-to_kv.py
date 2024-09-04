406e7a868cc2ecd1bc87da92d28e342686f44c06
mulimuoki001 <b.muoki@alustudent.com>
2024-09-04 13:56:52 +0300

    tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 
+from typing import Union, Tuple
+
 """
     This script calculates the factorial of a number.
     It takes an integer as input and returns a tuple
@@ -8,6 +10,6 @@
 """
 
 
-def to_kv(k: str, v: int | float) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Creates a dictionary with keys k and values v."""
     return k, float(v) ** 2
