2ec30886a7041ca8aae0bc04c298fc5ae55c28da
mulimuoki001 <b.muoki@alustudent.com>
2024-09-04 14:13:24 +0300

    annotation of a function

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""Import List and Tuple"""
+from typing import List, Tuple
+
+"""
+This module contains a function that
+returns a list of tuples, where each tuple
+contains an element from the input list and its length.
+
+Example:
+    >>> element_length([[1, 2, 3], [4, 5], [6]])
+    [([1, 2, 3], 3), ([4, 5], 2), ([6], 1)]
+"""
+
+
+def element_length(lst: List[List[any]]) -> List[Tuple[List[any], int]]:
+    """
+    Returns a list of tuples, where each tuple contains an element from the input list and its length.
+
+    Args:
+        lst (List[List[Any]]): The input list.
+
+    Returns:
+        List[Tuple[List[Any], int]]: The list of tuples.
+
+    Example:
+        >>> element_length([[1, 2, 3], [4, 5], [6]])
+        [([1, 2, 3], 3), ([4, 5], 2), ([6], 1)]
+    """
+    return [(i, len(i)) for i in lst]
