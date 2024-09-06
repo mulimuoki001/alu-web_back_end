0f710fff87fa74c9a3d8e674b7e9f67f850b129c
mulimuoki001 <b.muoki@alustudent.com>
2024-09-06 17:21:33 +0300

    asyncio task

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 
-    """
+"""
     A normal function that returns a coroutine object
-    """
+    that takes an integer as argument
+"""
 import asyncio
 import importlib
+
 wait_random = importlib.import_module("0-basic_async_syntax").wait_random
 """
 import the random and asyncio libraries
@@ -13,8 +15,10 @@ from the 0-basic_async_syntax module
 import wait_random function
 """
 
+
 def task_wait_random(max_delay: int):
     """
-    returns the float of a random delay
+    returns the an object of a random delay
     """
-    return asyncio.Task
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
