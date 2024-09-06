02500e64948e6c2b5152195d94799879dcfd8f0a
mulimuoki001 <b.muoki@alustudent.com>
2024-09-06 08:53:29 +0300

    random delays

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -10,5 +10,6 @@ import asyncio
 
 async def wait_random(max_delay: int = 10) -> float:
     """returns the float of a random delay"""
-    await asyncio.sleep(random.uniform(0, max_delay))
-    return random.uniform(0, max_delay)
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
