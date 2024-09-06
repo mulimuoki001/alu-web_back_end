97fab10e5f4f8acfa6372870ed9e22c6597d334f
mulimuoki001 <b.muoki@alustudent.com>
2024-09-06 19:51:52 +0300

    async comprehension

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
 
-
+"""measure runtime"""
 import time
 import importlib
 import asyncio
 
+"""import importlib module
+import asyncio module
+import time"""
 async_comprehension = importlib.import_module(
     "1-async_comprehension"
 ).async_comprehension
 
+"""
+import async_comprehension function from 1-async_comprehension file
+"""
+
 
 async def measure_runtime() -> float:
-    """Return the runtime of the code"""
+    """
+    Return the runtime of the code
+    """
     start = time.time()
     gathered = asyncio.gather(
         async_comprehension(),
