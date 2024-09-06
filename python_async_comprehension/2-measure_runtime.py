a87477160d234696df573d3384ec7390c630ca92
mulimuoki001 <b.muoki@alustudent.com>
2024-09-06 19:54:03 +0300

    async comprehension

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -22,12 +22,11 @@ async def measure_runtime() -> float:
     Return the runtime of the code
     """
     start = time.time()
-    gathered = asyncio.gather(
+    await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
     )
-    await gathered
     end = time.time()
     return end - start
