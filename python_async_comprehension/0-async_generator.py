55a692b421244b035e1926b5a2fdfd12519ffa51
mulimuoki001 <b.muoki@alustudent.com>
2024-09-07 22:25:12 +0300

    heyy

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,20 +1,8 @@
-#!/usr/bin/env python3
-
-"""async generator"""
-import asyncio
-import random
-from typing import Generator
-
-"""Import random and asyncio modules"""
-
-
-async def async_generator() -> Generator[float, None, None]:
-    """
-    Async generator
-    Lopps 10 times and
-    Yields random numbers between 0 and 10
-
-    """
-    for i in range(10):
-        await asyncio.sleep(1)
-        yield random.random()
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+    if i == 3:
+        break
+else:
+    print(0)
