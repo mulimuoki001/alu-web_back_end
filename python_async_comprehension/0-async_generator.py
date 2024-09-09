36369bfc6da99019a15cad72c59ca7c13ea69f70
mulimuoki001 <b.muoki@alustudent.com>
2024-09-10 00:28:46 +0300

    async generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,8 +1,14 @@
-i = 0
-while i < 5:
-    print(i)
-    i += 1
-    if i == 3:
-        break
-else:
-    print(0)
+#!/usr/bin/env python3
+
+"""async generator"""
+import random
+from typing import List
+
+
+async def async_generator() -> List[float]:
+    """The coroutine will collect 10 random numbers
+    using an async comprehensing over
+    async_generator, then return the 10 random numbers.
+    """
+    list1 = [random.random() for _ in range(10)]
+    return list1
