b7408acb704cc6de4e4d6b4e6c89e608b2613598
mulimuoki001 <b.muoki@alustudent.com>
2024-09-12 16:08:45 +0300

    re.sub filter for log

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -25,13 +25,3 @@ def filter_datum(
             value = message.split(field + "=")[1].split(";")[0]
         message = re.sub(value, redaction, message)
     return message
-
-
-fields = ["password", "date_of_birth"]
-messages = [
-    "name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
-    "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;",
-]
-
-for message in messages:
-    print(filter_datum(fields, "xxx", message, ";"))
