1404b9b3bbf82a897b6b78432cb9d02a25489271
mulimuoki001 <b.muoki@alustudent.com>
2024-09-14 16:11:55 +0300

    encrypt password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Hash password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validate password"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
