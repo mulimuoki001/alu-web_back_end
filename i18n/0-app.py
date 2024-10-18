36b7299d858226112488be99627ef9110bc0a3b2
mulimuoki002 <mulimuoki001@gmail.com>
2024-10-18 15:41:08 +0300

    flask babel

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,6 +1,19 @@
+#!/usr/bin/venv python3
 from flask import Flask, render_template
+from flask_babel import Babel
+
+babel = Babel()
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
+app.config.from_objects(Config)
+babel.init_app(app)
 
 
 @app.route("/")
