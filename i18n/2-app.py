a6c11037f4ca15e54a9f62d9c9c3e45f43d4cb25
mulimuoki002 <mulimuoki001@gmail.com>
2024-10-18 23:36:47 +0300

    get a locale from a request

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """Import necessary modules"""
-from flask import Flask, request
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 """
@@ -15,6 +15,49 @@ The Flask application instance.
 """
 
 
+class Config:
+    """
+    Configuration class for the Flask app.
+
+    Attributes:
+        LANGUAGES (list): List of available languages.
+        BABEL_DEFAULT_LOCALE (str): Default locale.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone.
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+"""
+Create a Flask app and configure it with the Config class.
+"""
+app = Flask(__name__)
+app.config.from_object(Config)
+babel.init_app(app)
+
+
+def index():
+    """
+    Render the index.html template.
+
+    Returns:
+        str: Rendered HTML template.
+    """
+    return render_template("2-index.html")
+
+
+"""
+Route for the root URL.
+"""
+app.route("/")(index)
+
+"""
+Run the app if this module is executed directly.
+"""
+
+
 @babel.localeselector
 def get_locale():
     """
@@ -24,3 +67,7 @@ def get_locale():
     :return: The best match for the user's locale (e.g. 'en', 'fr')
     """
     return request.accept_languages.best_match(["en", "fr"])
+
+
+if __name__ == "__main__":
+    app.run()
