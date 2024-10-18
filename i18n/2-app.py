965a5bf3b9e9373d2545722d25e204753306fde4
mulimuoki002 <mulimuoki001@gmail.com>
2024-10-18 23:23:46 +0300

    get a locale from a request

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -2,7 +2,8 @@ from flask import Flask, request
 from flask_babel import Babel
 
 """
-This module initializes the Flask application and sets up the Babel instance for internationalization.
+This module initializes the Flask application
+and sets up the Babel instance for internationalization.
 """
 
 app = Flask(__name__)
@@ -13,7 +14,8 @@ The Flask application instance.
 
 def get_locale():
     """
-    Returns the best match for the user's locale based on the Accept-Language header.
+    Returns the best match for the user's locale
+    based on the Accept-Language header.
 
     :return: The best match for the user's locale (e.g. 'en', 'fr', 'es')
     """
@@ -24,5 +26,6 @@ def get_locale():
 
 babel = Babel(app, locale_selector=get_locale)
 """
-The Babel instance for internationalization, initialized with the Flask application and locale selector function.
+The Babel instance for internationalization, initialized
+with the Flask application and locale selector function.
 """
